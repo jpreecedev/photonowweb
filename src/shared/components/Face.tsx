97e15f21ac9e4e72aabd101e5ebda9c4b4e53f34
Jon Preece <jonpreece@hotmail.co.uk>
2019-08-28 12:51:07 +0100

    Convert base64 image to blob

--- src/shared/components/Face.tsx
+++ src/shared/components/Face.tsx
@@ -2,26 +2,42 @@ import React from 'react'
 import CssBaseline from '@material-ui/core/CssBaseline'
 import Typography from '@material-ui/core/Typography'
 import Container from '@material-ui/core/Container'
+import Button from '@material-ui/core/Button'
 import Paper from '@material-ui/core/Paper'
 
 import { withStyles } from '@material-ui/styles'
+import GridContainer from './GridContainer'
+import GridItem from './GridItem'
 
 const styles = theme => ({
   paper: {
-    marginTop: theme.spacing(8),
-    display: 'flex',
-    flexDirection: 'column',
-    alignItems: 'center',
-    padding: theme.spacing(2),
-    [theme.breakpoints.up(600 + theme.spacing(3) * 2)]: {
-      padding: theme.spacing(3)
-    }
+    padding: theme.spacing(3, 2)
   }
 })
 
+const b64toBlob = (b64Data, contentType = '', sliceSize = 512) => {
+  const byteCharacters = atob(b64Data)
+  const byteArrays = []
+
+  for (let offset = 0; offset < byteCharacters.length; offset += sliceSize) {
+    const slice = byteCharacters.slice(offset, offset + sliceSize)
+
+    const byteNumbers = new Array(slice.length)
+    for (let i = 0; i < slice.length; i++) {
+      byteNumbers[i] = slice.charCodeAt(i)
+    }
+
+    const byteArray = new Uint8Array(byteNumbers)
+    byteArrays.push(byteArray)
+  }
+
+  const blob = new Blob(byteArrays, { type: contentType })
+  return blob
+}
+
 const videoConstraints = {
-  width: 1280,
-  height: 720,
+  width: 720,
+  height: 576,
   facingMode: 'user'
 }
 
@@ -49,8 +65,11 @@ class Face extends React.Component {
   }
 
   capture = () => {
-    const imageSrc = this.webcam.getScreenshot()
-    console.log(imageSrc)
+    const imageSrc = this.webcam.getScreenshot().split(',')
+
+    const contentType = 'image/jpeg'
+    const blob = b64toBlob(imageSrc[1], contentType)
+    console.log(blob)
   }
 
   render() {
@@ -66,21 +85,34 @@ class Face extends React.Component {
     }
 
     return (
-      <Container component="main" maxWidth="sm">
+      <Container component="main">
         <CssBaseline />
         <Paper className={classes.paper}>
           <Typography component="h1" variant="h5">
             Show us your face...
           </Typography>
-          <WebcamInstance
-            audio={false}
-            height={videoConstraints.height}
-            ref={this.setRef}
-            screenshotFormat="image/jpeg"
-            width={videoConstraints.width}
-            videoConstraints={videoConstraints}
-          />
-          <button onClick={this.capture}>Capture photo</button>
+          <GridContainer alignItems="center">
+            <GridItem>
+              <WebcamInstance
+                audio={false}
+                height={videoConstraints.height}
+                ref={this.setRef}
+                screenshotFormat="image/jpeg"
+                width={videoConstraints.width}
+                videoConstraints={videoConstraints}
+              />
+            </GridItem>
+            <GridItem>
+              <Button
+                size="large"
+                color="primary"
+                variant="contained"
+                onClick={this.capture}
+              >
+                Capture photo
+              </Button>
+            </GridItem>
+          </GridContainer>
         </Paper>
       </Container>
     )
