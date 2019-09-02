f05557697e0a699b84368081aa8174f615e1f469
Jon Preece <jonpreece@hotmail.co.uk>
2019-09-02 12:47:32 +0100

    Removed addMomentToOrder, unused

--- src/server/database/basket/index.ts
+++ src/server/database/basket/index.ts
@@ -1,5 +1,4 @@
 import { getOrderForCustomer, userHasOrder } from './get'
 import { createOrder } from './create'
-import { addMomentToOrder } from './update'
 
-export { getOrderForCustomer, userHasOrder, createOrder, addMomentToOrder }
+export { getOrderForCustomer, userHasOrder, createOrder }
